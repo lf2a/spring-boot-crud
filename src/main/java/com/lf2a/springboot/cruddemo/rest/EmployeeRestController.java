e9e00b82f2c90d232ccca4bbc18d3d818286dbc4
Luiz Filipy <luizfilipy.aa@outlook.com>
2020-09-07 12:37:05 -0300

    get single employee method and save employee method

--- src/main/java/com/lf2a/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/lf2a/springboot/cruddemo/rest/EmployeeRestController.java
@@ -3,9 +3,7 @@ package com.lf2a.springboot.cruddemo.rest;
 import com.lf2a.springboot.cruddemo.entity.Employee;
 import com.lf2a.springboot.cruddemo.service.IEmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +20,25 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return iEmployeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee employee = iEmployeeService.findById(employeeId);
+
+        if (employee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        employee.setId(0);
+
+        iEmployeeService.save(employee);
+
+        return employee;
     }
 }
