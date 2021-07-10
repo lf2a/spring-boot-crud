164f954be1adc7ad7a81110cb1be404b093bca6e
Luiz Filipy <luizfilipy.aa@outlook.com>
2021-07-10 15:21:53 -0300

    Testando a atualizando codigo

--- src/main/java/com/lf2a/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/lf2a/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -1,48 +1,45 @@
-//package com.lf2a.springboot.cruddemo.service;
-//
-//import com.lf2a.springboot.cruddemo.dao.EmployeeRepository;
-//import com.lf2a.springboot.cruddemo.dao.IEmployeeDAO;
-//import com.lf2a.springboot.cruddemo.entity.Employee;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Qualifier;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//@Service
-//public class EmployeeServiceImpl implements IEmployeeService {
-//    private EmployeeRepository employeeRepository;
-//
-//    @Autowired
-//    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
-//        this.employeeRepository = employeeRepository;
-//    }
-//
-//    @Override
-//    public List<Employee> findAll() {
-//        return employeeRepository.findAll();
-//    }
-//
-//    @Override
-//    public Employee findById(int id) {
-//        Optional<Employee> result = employeeRepository.findById(id);
-//
-//        if (result.isPresent()) {
-//            return result.get();
-//        } else {
-//            throw new RuntimeException("Did not find employee id - " + id);
-//        }
-//    }
-//
-//    @Override
-//    public void save(Employee employee) {
-//        employeeRepository.save(employee);
-//    }
-//
-//    @Override
-//    public void deleteById(int id) {
-//        employeeRepository.deleteById(id);
-//    }
-//}
+package com.lf2a.springboot.cruddemo.service;
+
+import com.lf2a.springboot.cruddemo.dao.EmployeeRepository;
+import com.lf2a.springboot.cruddemo.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements IEmployeeService {
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee findById(int id) {
+        Optional<Employee> result = employeeRepository.findById(id);
+
+        if (result.isPresent()) {
+            return result.get();
+        } else {
+            throw new RuntimeException("Did not find employee id - " + id);
+        }
+    }
+
+    @Override
+    public void save(Employee employee) {
+        employeeRepository.save(employee);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        employeeRepository.deleteById(id);
+    }
+}
